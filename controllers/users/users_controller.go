0c62b8afecfea925e92147d4214b382a1c37daa3
Pablo Mendez <mendez.developer@gmail.com>
2020-08-11 17:46:06 -0300

    added materials from chapter

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,10 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/mendezdev/bookstore_users-api/domain/users"
+	"github.com/mendezdev/bookstore_users-api/services"
+	"github.com/mendezdev/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveError)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
