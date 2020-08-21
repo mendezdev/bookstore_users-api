006e1fdf0a4d3c310e7c678bd886b8da94d7f57a
Pablo Mendez <mendez.developer@gmail.com>
2020-08-20 22:00:12 -0300

    added db connection

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/mendezdev/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/mendezdev/bookstore_users-api/utils/date_utils"
 	"github.com/mendezdev/bookstore_users-api/utils/errors"
 )
@@ -12,6 +13,9 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
